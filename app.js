253fe550e42d72720f295cb39eb7b0697826c0e0
Hind El Gharbi <hind.elgharbi@live.fr>
2021-06-11 10:18:56 +0200

    authentification + ajout sauce

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const path = require('path');
 
-mongoose.connect('mongodb+srv://hind_elgharbi:Makeba3@cluster0.9ggbm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
-    {   useNewUrlParser: true,
-        useUnifiedTopology: true })
-    .then(() => console.log('Connexion à MongoDB réussie !'))
-    .catch(() => console.log('Connexion à MongoDB échouée !'));
+const sauceRoutes = require('./routes/sauces');
+const userRoutes = require('./routes/user');
 
 const app = express();
+
+app.use(bodyParser.json());
+
+mongoose.connect('mongodb+srv://So_pekocko:So7arYe6er@cluster0.9ggbm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => console.log('Connexion à MongoDB réussie !'))
+    .catch(() => console.log('Connexion à MongoDB échouée !'));
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
@@ -16,5 +24,10 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
 });
+
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
+app.use('/api/sauces', sauceRoutes);
+app.use('/api/auth', userRoutes);
 
 module.exports = app;
